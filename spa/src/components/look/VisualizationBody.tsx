a38cf047a1350b1664d3e5858fa72fd468f52d57
AppSiDaL <gilielcrack343@gmail.com>
2024-03-26 19:21:04 -0600

    Add VisualizationBody component

---
+++ spa/src/components/look/VisualizationBody.tsx
@@ -0,0 +1,10 @@
+import CustomLineChart from "../../charts/CustomLineChart";
+import React from "react";
+
+export default function VisualizationBody() {
+  return (
+    <div>
+      <CustomLineChart />
+    </div>
+  );
+}

5209cbfa869fb2d8e9e767160cbe321b4864a911
AppSiDaL <gilielcrack343@gmail.com>
2024-03-26 19:21:00 -0600

    Add Visualization component

---
+++ spa/src/components/look/Visualization.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import VisualizationHeader from './VisualizationHeader'
+import VisualizationBody from './VisualizationBody'
+
+export default function Visualization() {
+  return (
+    <div>
+        <VisualizationHeader />
+        <VisualizationBody />
+    </div>
+  )
+}
